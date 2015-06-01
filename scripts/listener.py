b01b9ae272dead7d3a3bb74a46cfd87e5719ebf7
Coloss <danekradek@gmail.com>
2015-06-01 18:31:47 +0200

    Submission cleanup

--- scripts/listener.py
+++ scripts/listener.py
@@ -47,8 +47,7 @@ class kinect_listener:
         # initializie listeners
         self.sub_rgb = rospy.Subscriber('/camera/rgb/image_rect_color', Image, self.callback_rgb)
         self.sub_occlusion = rospy.Subscriber('/ar_single_board/occlusion_mask', Image, self.callback_occlusion)
-        self.sub_depth = rospy.Subscriber('/camera/depth/image', Image, self.callback_depth)
-        #self.sub_pose = rospy.Subscriber('/ar_single_board/pose', PoseStamped, self.callback_pose)
+        #self.sub_depth = rospy.Subscriber('/camera/depth/image', Image, self.callback_depth)
         self.sub_modelview = rospy.Subscriber('/ar_single_board/modelview', Float32MultiArray, self.callback_modelview)
         self.sub_cam_info = rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, self.callback_cam)
         self.sub_ir = rospy.Subscriber('/IR_data', Image, self.callback_ir)
@@ -75,7 +74,7 @@ class kinect_listener:
         thread.start()
 
 
-
+    # TODO: remove
     def callback_cam(self, data):
         if self.P is None:
             # print data.P
@@ -85,7 +84,7 @@ class kinect_listener:
             #K = np.reshape(np.array(data.K), [3, 3])
             #np.save("/home/radek/catkin_ws/K.npy", K)
 
-
+    # load projection matrix into the engine
     def loadCamera(self):
         if self.P is not None:
             # print self.P
@@ -99,6 +98,7 @@ class kinect_listener:
             glP = glP.flatten()
             self.game.projection = glP.view()
 
+    # pass occlusion mask to the game
     def callback_occlusion(self,data):
         self.game.flip = True
         #print "mask"
@@ -120,8 +120,7 @@ class kinect_listener:
             print e
             return
 
-
-
+    # input detection
     def callback_ir(self,data):
         # print "IR image arrived"
         if not self.game.ready:
@@ -129,14 +128,13 @@ class kinect_listener:
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data)
             #self.currentFrame = cv_image
-            cv2.imwrite("/home/radek/catkin_ws/ir_image.png", cv_image)
+            #cv2.imwrite("/home/radek/catkin_ws/ir_image.png", cv_image)
         except CvBridgeError, e:
             print e
             return
         if self.display_stuff:
             cv2.imshow("IR window", cv_image)
             cv2.waitKey(3)
-
 
         # Set up the detector with default parameters.
         params = cv2.SimpleBlobDetector_Params()
@@ -159,22 +157,35 @@ class kinect_listener:
         # cv_image = cv2.resize(cv_image,(0,0), fx=10, fy=10) #(cv_image, (640,640)) #, interpolation=cv2.INTER_NEAREST)
         cv_image = cv2.resize(cv_image, (640,480))
 
-        # IR CAMERA CALIBRATION MATRIX
-        # K = np.matrix([[757.164773, 0, 335.313573], [0, 823.419897, 138.458047], [0, 0, 1]])
+        # IR CAMERA CALIBRATION MATRIX - INSERT ACCURATE CALIBRATION HERE
+        K = np.matrix([[757.164773, 0, 335.313573], [0, 823.419897, 138.458047], [0, 0, 1]])
+
+        # RGB TO IR RIGID MOTION MATRIX - INSERT ACCURATE MATRIX HERE
+        rgb2ir = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+        # get Rt matrix of the IR camera
+        Rt_ir = rgb2ir*self.Rt
+        T = Rt_ir[:, 3]
+
+        R =np.matrix(Rt_ir[0:3, 0:3])
+        KRinv = np.linalg.inv(np.matrix(K)*R)
+        RT = R.transpose()
+
+        # GET CAMERA CENTER in world space
+        C = - RT * T
+
+        # IR CAMERA DISTORTION COEFFICIENTS
+        distortion = np.matrix([-0.321378, -0.018411, -0.012105, -0.003352, 0])
         #
-        # # IR CAMERA DISTORTION COEFFICIENTS
-        # distortion = np.matrix([-0.321378, -0.018411, -0.012105, -0.003352, 0])
-        #
-        # # UNDISTORT THE IR IMAGE
-        # cv_image = cv2.undistort(cv_image, K, distortion)
+        # UNDISTORT THE IR IMAGE
+        cv_image = cv2.undistort(cv_image, K, distortion)
 
         # BLOB DETECTION
         keypoints = detector.detect(cv_image)
 
-        # P = np.matrix(self.K)*np.matrix(self.Rt)
-        # R = np.matrix(self.Rt[0:3,0:3])
-        # RT = R.transpose()
-
+        # get inverse of the IR camera matrix
+        retval, PIrInv = cv2.invert(Pir, flags=cv2.DECOMP_SVD)
+        PIrInv = np.matrix(PIrInv)
 
         for kp in keypoints:
             x = int(kp.pt[1])
@@ -182,138 +193,58 @@ class kinect_listener:
 
             timestamp = int(time.time())
 
-            # if (timestamp - self.timestamp) < 5000 and \
-            #         abs(self.game.inputIRX - x) < 10 and \
-            #         abs(self.game.inputIRY - y) < 10:
-            #     print "input discarded"
-            #     continue
-            # self.timestamp = timestamp
+            if (timestamp - self.timestamp) < 5000 and \
+                abs(self.game.inputIRX - x) < 10 and \
+                abs(self.game.inputIRY - y) < 10:
+                #print "input discarded - too soon"
+                continue
+            self.timestamp = timestamp
 
+            pt = np.matrix([[y], [x], [1]])
 
+            # back-project detected point, to obtain a point somewhere on the ray
+            PX = np.matrix(PIrInv)*np.matrix(pt)
 
+            # unhomogenize
+            PX = PX[:]/PX[3]
 
-            # pass input to the game
-            #print str(x) + "  " + str(y)
-            #self.game.inputIRX = x
-            #self.game.inputIRY = y
-            #self.game.inputIRReady = True
-            #glutPostRedisplay()
+            # get center of the camera in board (world space)
+            C = Rt * np.matrix([[0], [0], [0], [1]])
+            C[1] = -C[1]
 
-            # RT matrix of RGB camera, use this one for now
-            # # TODO: add IR camera offset
-            # Rt = np.matrix(self.Rt)
-            #
-            # # P = K[R|t] is the camera matrix of IR camera
-            # P = np.matrix(self.K)*np.matrix(Rt)
-            #
-            # # get pseudoinverse of Pir for backprojection
-            # #PirInv = cv2.invert(Pir)
-            #
-            # new_transform = np.ndarray(P.shape, np.float32)
-            # new_transform[:] = P[:]
-            # P_conv = new_transform
-            # retval, PInv = cv2.invert(P_conv, flags=cv2.DECOMP_SVD)
-            # #PInv = np.linalg.pinv(P)
-            # PInv = np.matrix(PInv)
-            #
-            # PX = np.matrix(PInv)*np.matrix(pt)
-            # PX = PX[:]/PX[3]
-            #
-            # direc = PX[0:3] - C
-            # intersectionplane = 2
-            # t2 = -C.item(intersectionplane)/direc.item(intersectionplane)
-            # planeX = dPoint.item(0)
-            # planeY = dPoint.item(1)
+            # get the ray direction vector
+            direc = PX[0:3] - C
 
+            intersectionplane = 2
+            t2 = -C.item(intersectionplane)/direc.item(intersectionplane)
 
-            # PirInv = np.linalg.pinv(Pir)
-            #
-            # # detected point in pixel coords
-            # pt = np.matrix([[x], [y], [1]])
-            #
-            # # back-project detected point
-            # PX = np.matrix(PirInv)*np.matrix(pt)
-            # # unhomogenize
-            # PX = PX[0:3]/PX[3]
-            #
-            # # get center of the camera in board (world space)
-            # C = Rt * np.matrix([[0], [0], [0], [1]])
-            #
-            # # ray has to pass through between C and PX
-            # ray = (C - PX)
-            # # to non-homogeneous
-            # ray = ray[0:3]
-            # # normalize the ray
-            # ray = ray / np.linalg.norm(ray)
-            #
-            # # intersection with xy plane
-            # t = C[2]/ray[2]
-            #
-            # # find out coordinates on the xy-plane
-            # planeX = (t*ray[0] + C[0]).item(0)
-            # planeY = (t*ray[1] + C[1]).item(0)
+            # get the intersection points
+            dPoint = (t2 * direc) + C
+            planeX = dPoint.item(0)
+            planeY = dPoint.item(1)
 
-            # is input valid?
-            # print "---"
-            # if planeX > 5 or planeX < -5:
-            #     print "wrong x"
-            #     print str(planeX) + " " + str(planeY)
-            #     print Rt
-            #     break
-            # if planeY > 5 or planeY < -5:
-            #     print "wrong y"
-            #     print str(planeX) + " " + str(planeY)
-            #     print Rt
-            #     break
-            # if (timestamp - self.timestamp) < 8000 and \
-            #         abs(self.game.inputIRX - planeX) < 0.1 and \
-            #         abs(self.game.inputIRY - planeY) < 0.1:
-            #     print "input discarded"
-            #     continue
-            # self.timestamp = timestamp
-            #
-            # # pass input to the game
-            # #print str(x) + "  " + str(y)
-            # self.game.inputIRX = planeX
-            # self.game.inputIRY = planeY
-            # self.game.inputIRReady = True
-            # glutPostRedisplay()
-            # print Rt
-            # print planeX
-            # print planeY
-            # self.f.write( str(planeX) + ' ' + str(planeY) + "\n")
-
+            # pass the input to the game for evaluation
+            self.game.clickedCoordinates = (planeX, planeY, 0.0)
+            self.game.inputIR = True
             break
 
-
-        # print keypoints
         # Draw detected blobs as red circles.
         # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
         im_with_keypoints = cv2.drawKeypoints(cv_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        # Show keypoints
-        #resized = cv2.resize(im_with_keypoints, (0,0), fx=10, fy=10)
+        # visualize the keypoints
         cv2.resizeWindow("Keypoints",640,480)
         cv2.imshow("Keypoints", im_with_keypoints)
+        cv2.waitKey(1)
 
-        cv2.waitKey(3)
 
-
-    def callback_pose(self, data):
-        if not self.game.ready:
-            return
-
-        self.loadCamera()
-        glutPostRedisplay()
-
+    # load current pose as modelview matrix into the engine
     def callback_modelview(self, data):
         if not self.game.ready:
             return
         self.loadCamera()
         self.game.modelview = np.array(data.data).flatten() # array(data.data, dtype=float)
         glutPostRedisplay()
-        #print "Modelview: "
-        #print self.game.modelview[-4:-1]
 
     def callback_position(self, data):
         print "incoming position!"
@@ -333,12 +264,9 @@ class kinect_listener:
             return
         self.Rt = np.matrix(Rt.copy())
         self.Pcv = self.K*self.Rt
-        #outfile = TemporaryFile()
-        np.save("/home/radek/catkin_ws/Rt.npy", self.Rt)
-        #C = Rt * np.matrix([[0], [0], [0], [1]])
-        #print C.transpose()
-        #print self.Rt
+        #np.save("/home/radek/catkin_ws/Rt.npy", self.Rt)
 
+    # get K matrix
     def callback_K(self, data):
         if self.K is not None:
             return
@@ -350,33 +278,18 @@ class kinect_listener:
             return
         print "GOT K!"
         self.K = np.matrix(K.copy())
-        np.save("/home/radek/catkin_ws/K.npy", self.K)
-        #print self.Rt
+        #np.save("/home/radek/catkin_ws/K.npy", self.K)
 
-
-
+    # pass RGB video frame into game engine
     def callback_rgb(self, data):
         if self.game.ready:
             try:
                 cv_image = self.bridge.imgmsg_to_cv2(data, "rgb8")
 
                 cvHeight, cvWidth, _ = cv_image.shape
-                cv2.imwrite("/home/radek/catkin_ws/image.png", cv_image)
-                #print str(self.game.height) + ", " + str(self.game.width)
-                #print str(cvHeight) + ", " + str(cvWidth)
-
-                #newX = float(self.game.width) / float(cvWidth)
-                #newY = float(self.game.height) / float(cvHeight)
-                #print "scaleX = " + str(newX)
-                #print "scaleY = " + str(newY)
-                #resizedFrame =, fx=newX, fy=newY)
-                #self.imY, self.imX, _ = resizedFrame.shape
-                # self.game.currentFrame = resizedFrame
 
                 self.imY, self.imX, _ = cv_image.shape
                 self.game.currentFrame = cv_image
-
-                #print resizedFrame.shape
 
                 self.game.newFrameArrived = True
                 glutPostRedisplay()
@@ -386,6 +299,7 @@ class kinect_listener:
                 cv2.imshow("RGB window", cv_image)
                 cv2.waitKey(1)
 
+    # not needed
     def callback_depth(self, data):
         try:
             cv_image_depth = self.bridge.imgmsg_to_cv2(data)
@@ -399,6 +313,7 @@ class kinect_listener:
         self.game.start()
 
 
+# initialize ros game node
 def listener():
     kl = kinect_listener()
     rospy.init_node('kinect_listener', anonymous=True)
